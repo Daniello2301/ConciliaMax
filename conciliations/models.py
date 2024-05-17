7100f4076c24b166e5d9d02aa42b7982d2fdd0a4
Daniel Lopera Gómez <92329281+Daniello2301@users.noreply.github.com>
2024-05-17 14:55:51 -0500

    config files for futures request

--- conciliations/models.py
+++ conciliations/models.py
@@ -43,7 +43,7 @@ class Conciliation(models.Model):
     date = models.CharField(max_length=50,null=True)
     bank = models.CharField(max_length=50,null=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2, null=True)
-    BookId = models.ForeignKey(BookData, on_delete=models.CASCADE)
+    BookId = models.ForeignKey(BookData, on_delete=models.CASCADE,)
     BankId = models.ForeignKey(BankData, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
